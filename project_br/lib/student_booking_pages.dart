9a9a83d2bff81be6a6d11617e463e37e664507b4
Nurulhuda Hd <nurulhudaadamishaq@gmail.com>
2025-10-26 16:16:57 +0700

    feat: My booking page

---
+++ project_br/lib/student_booking_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentBookingPages extends StatefulWidget {
+  const StudentBookingPages({super.key});
+
+  @override
+  State<StudentBookingPages> createState() => _StudentBookingPagesState();
+}
+
+class _StudentBookingPagesState extends State<StudentBookingPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
