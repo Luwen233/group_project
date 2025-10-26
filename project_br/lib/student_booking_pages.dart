bfa8dc552eced63cbf4b9e761951186a25009d04
Nurulhuda Hd <nurulhudaadamishaq@gmail.com>
2025-10-26 22:44:25 +0700

    feat: my books page's appbar

--- project_br/lib/student_booking_pages.dart
+++ project_br/lib/student_booking_pages.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class StudentBookingPages extends StatefulWidget {
@@ -10,6 +12,36 @@ class StudentBookingPages extends StatefulWidget {
 class _StudentBookingPagesState extends State<StudentBookingPages> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFF7F7F7),
+        elevation: 3,
+        shadowColor: Colors.black54,
+        title: Text(
+          'My Books',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(67),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Divider(thickness: sqrt1_2, height: 0),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'Upcoming',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xff3C9CBF),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
