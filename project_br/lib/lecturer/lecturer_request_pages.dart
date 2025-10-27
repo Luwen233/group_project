0b6cd333a31171dec98d4de34e4ff10c14c73250
Paweekorn <ram14032517@gmail.com>
2025-10-27 22:47:02 +0700

    feat: create lecturer request pages

---
+++ project_br/lib/lecturer/lecturer_request_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LecturerRequestPages extends StatefulWidget {
+  const LecturerRequestPages({super.key});
+
+  @override
+  State<LecturerRequestPages> createState() => _LecturerRequestPagesState();
+}
+
+class _LecturerRequestPagesState extends State<LecturerRequestPages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
