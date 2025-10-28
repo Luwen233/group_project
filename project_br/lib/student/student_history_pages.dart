cf913b715bf667490a72293b74e857ed837f6d92
Nurulhuda Hd <nurulhudaadamishaq@gmail.com>
2025-10-28 16:10:14 +0700

    feat: history appbar

--- project_br/lib/student/student_history_pages.dart
+++ project_br/lib/student/student_history_pages.dart
@@ -10,6 +10,32 @@ class StudentHistoryPages extends StatefulWidget {
 class _StudentHistoryPagesState extends State<StudentHistoryPages> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFF7F7F7),
+          elevation: 3,
+          shadowColor: Colors.black54,
+          title: Text(
+            'My Books',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(67),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Divider(thickness: 1, height: 0),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: TabBar(tabs: [Tab(text: 'All',)]),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
