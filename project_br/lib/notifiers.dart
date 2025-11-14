2a5be4612f03426658381a886fdc7e114fca2769
Nurulhuda Hd <nurulhudaadamishaq@gmail.com>
2025-11-15 00:22:36 +0700

    listener for switchs tap after add new room

--- project_br/lib/notifiers.dart
+++ project_br/lib/notifiers.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
 ValueNotifier<int> selectedPageNotifer = ValueNotifier(0);
+
+final reloadRoomsNotifier = ValueNotifier(0);
+
+enum UserRole { lecturer, staff }
+
+final tapPendingNotifier = ValueNotifier(1);
