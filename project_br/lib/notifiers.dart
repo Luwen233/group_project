9bfeea1172f7b07a32c7e556aace0ff826df4e83
Nurulhuda Hd <nurulhudaadamishaq@gmail.com>
2025-11-15 15:00:34 +0700

    updates

--- project_br/lib/notifiers.dart
+++ project_br/lib/notifiers.dart
@@ -4,6 +4,4 @@ ValueNotifier<int> selectedPageNotifer = ValueNotifier(0);
 
 final reloadRoomsNotifier = ValueNotifier(0);
 
-enum UserRole { lecturer, staff }
-
-final tapPendingNotifier = ValueNotifier(1);
+enum UserRole { lecturer, staff } // for pending 
